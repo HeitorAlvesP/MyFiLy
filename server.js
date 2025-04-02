75824347535866496955b89b54237080e0879a4e
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2025-04-02 17:18:26 -0300

    começar o CRUD

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ dotenv.config();
 
 const PORT = 3000;
 
+app.get('/',(req, res) => {
+    res.redirect(302, 'login.html')
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor ligado em http://localhost:${PORT}`);
 });
